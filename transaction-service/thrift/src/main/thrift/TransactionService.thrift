49fbe7750efb862ab4538fd2a1252135d9e64dcb
Leon Bunschoten <lbunschoten@marktplaats.nl>
2018-02-17 20:50:00 +0100

    Added method to retrieve recent transactions

--- transaction-service/thrift/src/main/thrift/TransactionService.thrift
+++ transaction-service/thrift/src/main/thrift/TransactionService.thrift
@@ -7,6 +7,8 @@ struct Transaction {
 service TransactionService {
     Transaction getTransaction(1: i32 id)
 
+    list<Transaction> getRecentTransactions(1: i32 limit)
+
     Transaction insertTransaction(1: Transaction transaction)
 
     void deleteTransaction(1: i32 id)
