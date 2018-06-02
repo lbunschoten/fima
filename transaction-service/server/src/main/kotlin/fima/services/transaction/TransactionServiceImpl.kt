9da6d66898dd9942d3dfc230b5a104d6d3632b30
Leon Bunschoten <leon@leonbunschoten.nl>
2018-06-02 17:19:01 +0200

    Added debug line

--- transaction-service/server/src/main/kotlin/fima/services/transaction/TransactionServiceImpl.kt
+++ transaction-service/server/src/main/kotlin/fima/services/transaction/TransactionServiceImpl.kt
@@ -12,6 +12,7 @@ class TransactionServiceImpl(private val transactionsRepository: TransactionsRep
                              private val toTransactionConverter: RawTransactionToTransactionConverter) : TransactionServiceGrpc.TransactionServiceImplBase() {
 
     override fun getTransaction(request: GetTransactionRequest, responseObserver: StreamObserver<GetTransactionResponse>) {
+        println("DEBUG: Get transaction")
         val response = GetTransactionResponse
                 .newBuilder()
                 .setTransaction(Transaction.newBuilder().setId(1))
