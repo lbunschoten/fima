5e8ef85bb9aef1b3c3d3da4531a08ea3c8a2077d
Leon Bunschoten <leon@leonbunschoten.nl>
2018-08-19 14:13:01 +0200

    Make sure the transaction import service starts

--- transaction-import-service/server/src/main/kotlin/fima/services/transactionimport/Main.kt
+++ transaction-import-service/server/src/main/kotlin/fima/services/transactionimport/Main.kt
@@ -2,18 +2,28 @@ package fima.services.transactionimport
 
 import fima.services.transaction.TransactionServiceGrpc
 import io.grpc.ManagedChannelBuilder
+import io.grpc.ServerBuilder
 
 
 fun main(args: Array<String>) {
     val transactionService: TransactionServiceGrpc.TransactionServiceBlockingStub = {
-        val channel = ManagedChannelBuilder.forAddress("localhost", 9997).usePlaintext(true).build()
+        val channel = ManagedChannelBuilder.forAddress("localhost", 9997).usePlaintext().build()
         TransactionServiceGrpc.newBlockingStub(channel)
     }()
 
-    TransactionImportServiceImpl(transactionService)
+    val server = ServerBuilder
+            .forPort(9997)
+            .addService(TransactionImportServiceImpl(
+                    transactionService = transactionService
+            ))
+            .build()
+
+    server.start()
+    println("Transaction import service started")
 
     Runtime.getRuntime().addShutdownHook(Thread { println("Ups, JVM shutdown") })
+    server.awaitTermination()
 
-    println("Transaction statistics service stopped")
+    println("Transaction import service stopped")
 }
 
