b8c762891151f3df3bae233fe4896b1239cbc46e
Leon Bunschoten <leon@leonbunschoten.nl>
2018-08-19 16:22:21 +0200

    Hardcode host and port

--- transaction-import-service/server/src/main/kotlin/fima/services/transactionimport/Main.kt
+++ transaction-import-service/server/src/main/kotlin/fima/services/transactionimport/Main.kt
@@ -6,8 +6,8 @@ import io.grpc.ServerBuilder
 
 
 fun main(args: Array<String>) {
-    val transactionServiceHost: String = System.getenv("TRANSACTION_SERVICE_HOST") ?: "transaction-service"
-    val transactionServicePort: Int = System.getenv("TRANSACTION_SERVICE_PORT")?.toInt() ?: 9997
+    val transactionServiceHost = "transaction-service"
+    val transactionServicePort = 9997
 
     val transactionService: TransactionServiceGrpc.TransactionServiceBlockingStub = {
         val channel = ManagedChannelBuilder.forAddress(transactionServiceHost, transactionServicePort).usePlaintext().build()
