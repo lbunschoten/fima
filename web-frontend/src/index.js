3f47b68997bb6f166c753a6ef57e41b19b35a47e
Leon Bunschoten <leon@leonbunschoten.nl>
2018-12-29 13:57:39 +0100

    Use hash history instead of browser history

--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { createHashHistory } from "history";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import AdminLayout from "layouts/Admin/Admin.jsx";
 
 import "assets/scss/black-dashboard-react.scss";
 import "assets/css/nucleo-icons.css";
 
-const hist = createBrowserHistory();
+const hist = createHashHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
+  <HashRouter history={hist}>
     <Switch>
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
-  </Router>,
+  </HashRouter>,
   document.getElementById("root")
 );
