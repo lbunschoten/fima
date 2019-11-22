1caef196e14ab515a412e14f52a8fd05986ed68e
Leon Bunschoten <leon@leonbunschoten.nl>
2019-11-22 17:51:16 +0100

    Formatted file

--- transaction-service/server/src/main/kotlin/fima/services/transaction/conversion/RawDateToDateConverter.kt
+++ transaction-service/server/src/main/kotlin/fima/services/transaction/conversion/RawDateToDateConverter.kt
@@ -4,13 +4,13 @@ import fima.domain.transaction.Date
 
 class RawDateToDateConverter : (Int) -> Date {
 
-    override fun invoke(rawDate: Int): Date {
-        return Date.newBuilder().run {
-            day = rawDate.toString().substring(6, 8).toInt()
-            month = rawDate.toString().substring(4, 6).toInt()
-            year = rawDate.toString().substring(0, 4).toInt()
-            build()
-        }
+  override fun invoke(rawDate: Int): Date {
+    return Date.newBuilder().run {
+      day = rawDate.toString().substring(6, 8).toInt()
+      month = rawDate.toString().substring(4, 6).toInt()
+      year = rawDate.toString().substring(0, 4).toInt()
+      build()
     }
+  }
 
 }
