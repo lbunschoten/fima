8eeecab44152e592cdb22a4a63597cc290034198
Leon Bunschoten <leon@leonbunschoten.nl>
2019-12-14 19:06:22 +0100

    Fixed rounding error

--- web-frontend/src/views/Dashboard.jsx
+++ web-frontend/src/views/Dashboard.jsx
@@ -650,7 +650,7 @@ class Dashboard extends React.Component {
                           <tr>
                             <td>{t.date}</td>
                             <td>{t.name}</td>
-                            <td className="text-center">&euro;{parseFloat(t.amount).toFixed(2)}</td>
+                            <td className="text-center">&euro;{t.amount / 100}</td>
                           </tr>
                         ))
                       }
