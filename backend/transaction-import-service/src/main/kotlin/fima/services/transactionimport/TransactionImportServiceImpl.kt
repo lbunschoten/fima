6471d37966e01e8967852976f812613e3800679e
Leon Bunschoten <leon@leonbunschoten.nl>
2020-12-27 20:43:02 +0100

    Added more debug logging

--- backend/transaction-import-service/src/main/kotlin/fima/services/transactionimport/TransactionImportServiceImpl.kt
+++ backend/transaction-import-service/src/main/kotlin/fima/services/transactionimport/TransactionImportServiceImpl.kt
@@ -14,6 +14,7 @@ class TransactionImportServiceImpl(private val transactionService: TransactionWr
   private val logger = LoggerFactory.getLogger(javaClass)
 
   override suspend fun importTransactions(request: ImportTransactionsRequest): ImportTransactionsResponse {
+    logger.info("Importing transactions: ${request.transactions}")
     StringReader(request.transactions).use { reader ->
       val csvReader = CsvToBeanBuilder<Transaction>(reader)
         .withSeparator(',')
