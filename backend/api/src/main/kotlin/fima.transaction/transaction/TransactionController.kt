b5b21a3dd95ea0a916a2a6c841f472ee4320c928
Leon Bunschoten <leon@leonbunschoten.nl>
2020-12-27 14:09:38 +0100

    Use RequestPart instead of RequestParam

--- backend/api/src/main/kotlin/fima.transaction/transaction/TransactionController.kt
+++ backend/api/src/main/kotlin/fima.transaction/transaction/TransactionController.kt
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import java.nio.charset.Charset
@@ -56,7 +57,7 @@ class TransactionController @Autowired constructor(
   }
 
   @PutMapping("/import")
-  suspend fun importTransactions(@RequestParam("transactions") transactions: MultipartFile) {
+  suspend fun importTransactions(@RequestPart("transactions") transactions: MultipartFile) {
     val request = ImportTransactionsRequest.newBuilder().setTransactions(String(transactions.bytes, Charset.forName("UTF-8"))).build()
     transactionImportService.importTransactions(request)
   }
