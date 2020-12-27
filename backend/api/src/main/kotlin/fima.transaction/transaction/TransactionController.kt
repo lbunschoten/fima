5b09596c6af71adb170d98019d19b85d4992d770
Leon Bunschoten <leon@leonbunschoten.nl>
2020-12-27 16:52:20 +0100

    Use a mono for the multipart file upload

--- backend/api/src/main/kotlin/fima.transaction/transaction/TransactionController.kt
+++ backend/api/src/main/kotlin/fima.transaction/transaction/TransactionController.kt
@@ -11,74 +11,77 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.MediaType
 import org.springframework.http.codec.multipart.FilePart
-import org.springframework.http.codec.multipart.Part
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
 import java.nio.charset.StandardCharsets
 import java.util.UUID
 
 @RestController
 class TransactionController @Autowired constructor(
-  private val transactionService: TransactionServiceGrpcKt.TransactionServiceCoroutineStub,
-  private val transactionImportService: TransactionImportServiceGrpcKt.TransactionImportServiceCoroutineStub
+    private val transactionService: TransactionServiceGrpcKt.TransactionServiceCoroutineStub,
+    private val transactionImportService: TransactionImportServiceGrpcKt.TransactionImportServiceCoroutineStub
 ) {
 
-  private val logger = LoggerFactory.getLogger(javaClass)
+    private val logger = LoggerFactory.getLogger(javaClass)
 
-  @CrossOrigin
-  @GetMapping("/{id}")
-  suspend fun getTransaction(@PathVariable("id") transactionId: UUID): Transaction {
-    val request = GetTransactionRequest.newBuilder().setId(transactionId.toString()).build()
+    @CrossOrigin
+    @GetMapping("/{id}")
+    suspend fun getTransaction(@PathVariable("id") transactionId: UUID): Transaction {
+        val request = GetTransactionRequest.newBuilder().setId(transactionId.toString()).build()
 
-    return transactionService.getTransaction(request).transaction.simple()
-  }
-
-  @CrossOrigin
-  @GetMapping("/recent")
-  suspend fun getRecentTransactions(@RequestParam("offset") offset: Int, @RequestParam("limit") limit: Int): List<Transaction> {
-    val request = GetRecentTransactionsRequest
-      .newBuilder()
-      .setOffset(offset)
-      .setLimit(limit)
-      .build()
-
-    return transactionService.getRecentTransactions(request).transactionsList.map { it.simple() }
-  }
-
-  @CrossOrigin
-  @GetMapping("/statistics")
-  suspend fun getStatistics(): List<TransactionStatistics> {
-    val startOfYear = MonthInYear.newBuilder().setMonth(1).setYear(2018)
-    val endOfYear = MonthInYear.newBuilder().setMonth(12).setYear(2018)
-    return transactionService
-      .getMonthlyStatistics(TransactionsStatisticsRequest.newBuilder().setStartDate(startOfYear).setEndDate(endOfYear).build())
-      .monthlyStatisticsList
-      .map { it.simple() }
-  }
-
-  @PutMapping("/import", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
-  suspend fun importTransactions(@RequestBody transactions: Part): Flux<String> {
-      return transactions.content().map { dataBuffer ->
-          val bytes = ByteArray(dataBuffer.readableByteCount())
-          dataBuffer.read(bytes)
-          DataBufferUtils.release(dataBuffer)
-
-          String(bytes, StandardCharsets.UTF_8)
-      }
-      .map(this::processAndGetLinesAsList)
-      .flatMapIterable { it }
-  }
-
-  private fun processAndGetLinesAsList(s: String): List<String> {
-    return s.lines().map {
-      logger.info(it)
-      it
+        return transactionService.getTransaction(request).transaction.simple()
     }
-  }
+
+    @CrossOrigin
+    @GetMapping("/recent")
+    suspend fun getRecentTransactions(@RequestParam("offset") offset: Int, @RequestParam("limit") limit: Int): List<Transaction> {
+        val request = GetRecentTransactionsRequest
+            .newBuilder()
+            .setOffset(offset)
+            .setLimit(limit)
+            .build()
+
+        return transactionService.getRecentTransactions(request).transactionsList.map { it.simple() }
+    }
+
+    @CrossOrigin
+    @GetMapping("/statistics")
+    suspend fun getStatistics(): List<TransactionStatistics> {
+        val startOfYear = MonthInYear.newBuilder().setMonth(1).setYear(2018)
+        val endOfYear = MonthInYear.newBuilder().setMonth(12).setYear(2018)
+        return transactionService
+            .getMonthlyStatistics(TransactionsStatisticsRequest.newBuilder().setStartDate(startOfYear).setEndDate(endOfYear).build())
+            .monthlyStatisticsList
+            .map { it.simple() }
+    }
+
+    @PutMapping("/import", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
+    suspend fun importTransactions(@RequestPart("transactions") transactions: Mono<FilePart>): Flux<String> {
+        return transactions.toFlux().flatMap {
+            it.content().map { dataBuffer ->
+                val bytes = ByteArray(dataBuffer.readableByteCount())
+                dataBuffer.read(bytes)
+                DataBufferUtils.release(dataBuffer)
+
+                String(bytes, StandardCharsets.UTF_8)
+            }
+        }
+            .map(this::processAndGetLinesAsList)
+            .flatMapIterable { it }
+    }
+
+    private fun processAndGetLinesAsList(s: String): List<String> {
+        return s.lines().map {
+            logger.info(it)
+            it
+        }
+    }
 }
