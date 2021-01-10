bbb9f0281b80a89fa522b34bbe3f07e46ce77217
Leon Bunschoten <leon@leonbunschoten.nl>
2021-01-10 16:55:13 +0100

    Switched to using gradle

---
+++ backend/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    kotlin("jvm") version "1.4.21" apply false
+    kotlin("plugin.serialization") version "1.4.21" apply false
+}
+
+allprojects {
+    repositories {
+        maven {
+            credentials {
+                username = properties["nexus_username"].toString()
+                password = properties["nexus_password"].toString()
+            }
+            url = uri(properties["nexus_url"].toString())
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+}
---
+++ backend/settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "fima"
+include(":transaction-service")
+include(":transaction-import-service")
+include(":domain")
+include(":api")
