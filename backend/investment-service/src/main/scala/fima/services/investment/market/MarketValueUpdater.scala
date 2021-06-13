ea4c622d6a0f0809e34abb5606834eee18c98843
Leon Bunschoten <leon@leonbunschoten.nl>
2021-06-13 12:47:54 +0200

    Make sure that queries actually get executed

--- backend/investment-service/src/main/scala/fima/services/investment/market/MarketValueUpdater.scala
+++ backend/investment-service/src/main/scala/fima/services/investment/market/MarketValueUpdater.scala
@@ -1,33 +1,42 @@
 package fima.services.investment.market
 
 import cats.effect.{IO, Resource}
+import cats.implicits._
 import doobie.Transactor
 import doobie.implicits._
+import fima.services.investment.ApiError
 import fima.services.investment.model.Stock.StockSymbol
 import fima.services.investment.repository.StockRepository
 
-class MarketValueUpdater(private val stockApi: StockApi,
-                         private val stockRepository: StockRepository,
-                         private val transactor: Resource[IO, Transactor[IO]]) {
+class MarketValueUpdater(
+  private val stockApi: StockApi,
+  private val stockRepository: StockRepository,
+  private val transactor: Resource[IO, Transactor[IO]]
+) {
 
   def updateMarketValues(): Unit = {
     println("Updating all market values")
     transactor
       .use { xa => stockRepository.findAll().transact(xa) }
-      .map { stocks =>
+      .flatMap { stocks =>
         stocks
           .map(_.symbol)
-          .toSet
-          .foreach(updateMarketValue)
+          .map(updateMarketValue)
+          .flatMap {
+            case Left(apiError) =>
+              println(apiError.message)
+              None
+            case Right(v) => Option(v)
+          }.sequence
       }
       .unsafeRunAsyncAndForget()
   }
 
-  def updateMarketValue(symbol: StockSymbol): Unit = {
+  def updateMarketValue(symbol: StockSymbol): Either[ApiError, IO[Int]] = {
     println(s"Updating market value for symbol: $symbol")
     stockApi.getTimeseries(symbol).map { timeseries =>
       val (_, timeSeries) = timeseries.timeseries.head
-      stockRepository.updateMarketValue(symbol, timeSeries.adjustedClose)
+      transactor.use { xa => stockRepository.updateMarketValue(symbol, timeSeries.adjustedClose).transact(xa) }
     }
   }
 
