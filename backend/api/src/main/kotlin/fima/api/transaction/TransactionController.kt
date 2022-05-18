7caa82ad75ea51680bba438c18e0f37afd998d0e
Leon Bunschoten <leon@leonbunschoten.nl>
2022-05-18 15:29:09 +0200

    Fixed port and clean up

--- backend/api/src/main/kotlin/fima/api/transaction/TransactionController.kt
+++ backend/api/src/main/kotlin/fima/api/transaction/TransactionController.kt
@@ -1,17 +1,21 @@
 package fima.api.transaction
 
-import fima.domain.transaction.monthInYear
-import fima.services.transaction.*
+import fima.services.transaction.ImportTransactionsRequest
+import fima.services.transaction.TransactionServiceGrpcKt
+import fima.services.transaction.importTransactionsRequest
+import fima.services.transaction.tagTransactionsRequest
 import kotlinx.coroutines.reactive.awaitSingle
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.ResponseEntity
 import org.springframework.http.codec.multipart.FilePart
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 import java.nio.charset.Charset
-import java.util.*
 
 @RestController
 class TransactionController @Autowired constructor(
@@ -19,46 +23,6 @@ class TransactionController @Autowired constructor(
 ) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
-
-    @CrossOrigin
-    @GetMapping("/transaction/{id}")
-    suspend fun getTransaction(@PathVariable("id") transactionId: UUID): Transaction {
-        val request = getTransactionRequest { id = transactionId.toString() }
-
-        return transactionService
-            .getTransaction(request)
-            .transaction
-            .let(Transaction::fromProto)
-    }
-
-    @CrossOrigin
-    @GetMapping("/transaction/recent")
-    suspend fun getRecentTransactions(@RequestParam("offset") offset: Int, @RequestParam("limit") limit: Int): List<Transaction> {
-        logger.info("Received request for recent transactions")
-
-        val request = getRecentTransactionsRequest { this.offset = offset; this.limit = limit }
-
-        return transactionService
-            .getRecentTransactions(request)
-            .transactionsList
-            .map(Transaction::fromProto)
-    }
-
-    @CrossOrigin
-    @GetMapping("/transaction/statistics")
-    suspend fun getStatistics(): List<MonthlyTransactionStatistics> {
-        logger.info("Received request for transaction statistics")
-
-        val startOfYear = monthInYear { month = 1; year = 2020 }
-        val endOfYear = monthInYear { month = 12; year = 2020 }
-        return transactionService
-            .getMonthlyStatistics(transactionsStatisticsRequest {
-                startDate = startOfYear
-                endDate = endOfYear
-            })
-            .monthlyStatisticsList
-            .map(MonthlyTransactionStatistics::fromProto)
-    }
 
     @GetMapping("/tag")
     suspend fun tagTransactions(): ResponseEntity<String> {
@@ -75,22 +39,15 @@ class TransactionController @Autowired constructor(
 
         val request: ImportTransactionsRequest = transactions
             .flatMap {
-                logger.info("Reached 1")
                 DataBufferUtils.join(it.content()).map { dataBuffer ->
-                    logger.info("Reached 2")
                     dataBuffer.asInputStream().use { input ->
-                        logger.info("Reached 3")
-                        val request = importTransactionsRequest {
+                        importTransactionsRequest {
                             this.transactions = String(input.readAllBytes(), Charset.forName("UTF-8"))
                         }
-                        logger.info("Reached 4")
-
-                        request
                     }
                 }
             }.awaitSingle()
 
-        logger.info("Reached 5")
         transactionService.importTransactions(request)
 
         return ResponseEntity.ok("Upload successful")
