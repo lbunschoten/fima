4d644456de148fbe1d73759c16da6f5401caa878
Leon Bunschoten <leon@leonbunschoten.nl>
2023-01-01 22:51:48 +0100

    Fixes linting warnings

--- web-frontend/src/index.tsx
+++ web-frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {HashRouter, Redirect, Route, Switch} from "react-router-dom";
-import {AnyAction, configureStore, getDefaultMiddleware, ThunkDispatch} from '@reduxjs/toolkit'
+import {AnyAction, configureStore, ThunkDispatch} from '@reduxjs/toolkit'
 
 import LayoutContainer from "./layouts/LayoutContainer";
 
